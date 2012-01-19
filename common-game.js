a68c184cd48afddd31edc4e3e6b323f43ec7843b
Matthew Smiglarski <matt@wks-e08.local>
2012-01-19 08:15:50 +0000

    Work off the levels object, do away with the other config object. Get swapping betweek question modes correctly.

--- common-game.js
+++ common-game.js
@@ -13,6 +13,14 @@ function row(n) {
 	return dst;
 }	
 
+function col(n) {
+	var dst = [];
+	for (var i=0; i<src.length; i++) {
+		dst[i] = tableValues[i][n];
+	}
+	return dst;
+}	
+
 function createChoiceWidget(answerCallback, key, value) {
 	var choiceEl = document.createElement("div");
 	var progressWrapper = document.createElement("div");
@@ -93,14 +101,18 @@ function createGame(config) {
 
 
 	game.addHook('correct', function() {
+			var questionEl = document.getElementById('questions').children[0];
 			document.getElementById('status').style.backgroundColor = 'green';
-			document.getElementById('questions').children[0].style.backgroundColor = 'green';
+			questionEl.parentNode.removeChild(questionEl);
 			window.setTimeout("document.getElementById('status').style.backgroundColor = ''", 300);
 			romajiPlugin.disable();
 			});
 	game.addHook('incorrect', function() {
+			var questionEl = document.getElementById('questions').children[0];
 			document.getElementById('status').style.backgroundColor = 'red';
-			document.getElementById('questions').children[0].style.backgroundColor = 'red';
+			questionEl.style.backgroundColor = 'red';
+			questionEl.parentNode.removeChild(questionEl);
+			document.getElementById('incorrectAnswers').appendChild(questionEl);
 			window.setTimeout("document.getElementById('status').style.backgroundColor = ''", 300);
 			romajiPlugin.enable();
 			});
@@ -140,22 +152,21 @@ var doubleClosure = (function () {
 
 var questionsHook = (function() {
 		var levels = [
+		{ options: row(2), questions: row(2), hints: false, mode: 'multichoice' },
 		{ options: row(0), questions: row(0), hints: true, mode: 'grid' },
 		{ options: row(1), questions: row(1), hints: false, mode: 'grid' },
-		{ options: row(2), questions: row(2), hints: false, mode: 'multichoice' }
+		{}
 		];
 		var i=0;	
-		var level;
 
-		var config = {};
 
 		function createMultiChoices(answerCallback, config) {
 		var ideograph = document.getElementById(currentValue);
 		var choicesEl = document.getElementById('choices');
-		var left = options;
+		var left = config.options;
 		var i=0;
 		var j, key, choiceEl;
-		var limit = options.length;
+		var limit = config.options.length;
 
 		ideograph && (ideograph.style.display = 'block');
 		choicesEl.innerHTML = '';
@@ -174,27 +185,29 @@ var questionsHook = (function() {
 			var questionEl;
 			var i,j,ideographWidget;
 
-			level = levels.shift();
-			console.log('Next level: ' + level);
+			var level = levels.shift();
 			mode = level.mode;
 			grid.clear();
 			for (var i=0; i<level.questions.length; i++) {
-				questionEl = createQuestionWidget(data[level.questions[i]]);
+				questionEl = createQuestionWidget(data[level.questions[level.questions.length-i-1]]);
 				statusBar.insertBefore(questionEl, statusBar.children[0]);
 			}
-			config.data = tableValues;
+			level.data = tableValues;
 			if (mode == 'grid') {
-				config.gridConfig = {};
+				level.gridConfig = {};
 			} else {
-				config.multiChoiceConfig = {}
+				level.multiChoiceConfig = {}
 			}
+			return level;
 		}
 
 		return (function() {
+				var level;
 
 				return function(answerCallback) {
 				if (!level || !level.questions.length) {
-				nextLevel();
+				level = nextLevel();
+				if (!level) return;
 				}
 
 				currentValue = level.questions.shift();
@@ -202,13 +215,13 @@ var questionsHook = (function() {
 				document.getElementById('grid').style.display = 'none';
 				document.getElementById('choices').style.display = 'none';
 
-				if (config.gridConfig) {
+				if (level.gridConfig) {
 				for (i=0; i<grid.rows(); i+=1) {
-				if (!config.restrictCol || col === config.restrictCol) {
+				if (!level.restrictCol || col === level.restrictCol) {
 				for (var j=0; j<grid.cols(); j+=1) {
-				if (!config.restrictRow || row === config.restrictRow) {
+				if (!level.restrictRow || row === level.restrictRow) {
 				var col = rightToLeft? grid.cols()-1-j : j;
-				ideographWidget = choiceElements[config.data[col][i]];
+				ideographWidget = choiceElements[level.data[col][i]];
 				grid.setValue(i, j, ideographWidget);
 				}
 				}
@@ -217,8 +230,8 @@ var questionsHook = (function() {
 				document.getElementById('grid').style.display = 'block';
 				}
 
-				if (config.multiChoiceConfig) {
-					createMultiChoices(answerCallback, {});
+				if (level.multiChoiceConfig) {
+					createMultiChoices(answerCallback, level);
 					document.getElementById('choices').style.display = 'block';
 				}
 				return currentValue;
